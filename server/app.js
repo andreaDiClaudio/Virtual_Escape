8661f7d13ba6503cbdac911cfe4be790ef95aeff
andreaDiClaudio <andreadicla@gmail.com>
2023-05-11 17:46:48 +0200

    Working authentication

--- server/app.js
+++ server/app.js
@@ -1,16 +1,14 @@
 import express from "express";
 
 const app = express();
-
 //for parsing the req.body
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send({ message: "Working" });
-})
-
 import userRouter from "./routers/userRouter.js";
 app.use(userRouter);
+
+import loginRouter from "./routers/loginRouter.js";
+app.use(loginRouter);
 
 const PORT = 8080;
 app.listen(PORT, () => {
---
+++ server/routers/loginRouter.js
@@ -0,0 +1,54 @@
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import db from "../database/connection.js";
+
+const router = Router();
+
+router.post("/login", async (req, res) => {
+    const { email, nickname, password } = req.body;
+
+    // Validate the required fields
+    if (!email || !password || !nickname) {
+        return res.status(400).json({ message: 'Please provide all required credentials' });
+    }
+
+    //Check if user exists
+    const [users, fields] = await db.execute('SELECT * FROM users WHERE email = ?', [email]);
+
+    if (users.length === 0) {
+        return res.status(400).json({ message: `Couldn't find user with the email '${email}. Try again with different credentials'` });
+    }
+
+    //saves the user in a variable
+    const user = users[0];
+
+    if (user) {
+        if (nickname != user.nickname)
+            return res.status(404).json({
+                message: `Couldn't find user with username '${nickname}' connected to email '${email}'`
+            });
+    }
+
+    /* //TODO when doing authorization
+    if (isSamePassword) {
+        //save the user info in the session
+        req.session.user = {
+            id: userFound.id,
+            username: userFound.username,
+            email: userFound.email
+        };
+        */
+
+    //Check if password is the same
+    const isSamePassword = await bcrypt.compare(password, user.password);
+
+    if (isSamePassword) {
+        return res.status(200).json({ message: "Logged in" });
+    }
+    else {
+        return res.status(400).json({ message: "Wrong password. Try again" });
+    }
+
+});
+
+export default router;
