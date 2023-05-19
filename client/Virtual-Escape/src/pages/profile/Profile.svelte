cd60b720cb9da9fd73245ae3e11788f075350e9f
andreaDiClaudio <andreadicla@gmail.com>
2023-05-19 15:04:13 +0200

    Started working on frontend profile section

--- client/Virtual-Escape/src/pages/profile/Profile.svelte
+++ client/Virtual-Escape/src/pages/profile/Profile.svelte
@@ -1,5 +1,24 @@
 <script>
     import Navbar from "../../components/Navbar.svelte";
+    import { onMount, onDestroy } from "svelte";
+    import { titleStore } from "../../stores/tabTitle/tabTitle.js";
+
+    titleStore.setTitle("Profile | VE");
+
+    onDestroy(() => {
+        titleStore.resetTitle();
+    });
 </script>
 
 <Navbar />
+
+<div id="profile-page">
+    <div id="left-panel">
+        <h2>Folders</h2>
+        <p>1. Horizon Forbidden West</p>
+        <p>1. A very long title of a none existing gam, but just in case</p>
+    </div>
+    <div id="right-panel">
+        <p>Profile section</p>
+    </div>
+</div>
